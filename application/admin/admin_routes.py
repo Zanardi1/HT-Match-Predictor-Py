46efd93d64ad744125835b21d41654391b3125e0
Bogdan Doicin <alex.zanardi@gmail.com>
2020-03-25 15:06:53 +0200

    Designing index page

--- application/admin/admin_routes.py
+++ application/admin/admin_routes.py
@@ -4,6 +4,6 @@ from flask import render_template
 admin_bp = Blueprint('admin_bp', __name__, template_folder='templates', static_folder='static')
 
 
-@admin_bp.route('/')
+@admin_bp.route('/admin/templates')
 def admin():
     return render_template('admin.html')
