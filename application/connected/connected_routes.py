f454a686beb70336ec230ec18478e3974d07816a
Bogdan Doicin <alex.zanardi@gmail.com>
2020-03-27 11:22:44 +0200

    Designing connected.html page

--- application/connected/connected_routes.py
+++ application/connected/connected_routes.py
@@ -1,9 +1,14 @@
 from flask import Blueprint
 from flask import render_template
+import global_library
 
 connected_bp = Blueprint('connected_bp', __name__, template_folder='templates', static_folder='static')
+ratings = global_library.ratings
+positions = global_library.positions
+statuses = global_library.statuses
 
 
-@connected_bp.route('/')
+@connected_bp.route('/connected/templates')
 def connected():
-    return render_template('connected.html', title="Connected to Hattrick")
+    return render_template('connected.html', title="Connected to Hattrick", from_index=False, ratings=ratings,
+                           positions=positions, statuses=statuses)
