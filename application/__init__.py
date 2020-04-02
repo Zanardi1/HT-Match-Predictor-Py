f477e08f726fa81274cdf0e1b3013062a476ddfd
Bogdan Doicin <alex.zanardi@gmail.com>
2020-04-02 13:42:30 +0300

    Experimentez scrierea in BD

--- application/__init__.py
+++ application/__init__.py
@@ -1,21 +1,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-from .admin import admin_routes
-from .connected import connected_routes
-from .index import index_routes
-
 db = SQLAlchemy()
 
 
 def create_app():
     app = Flask(__name__, template_folder="templates", static_folder="static")
     app.config.from_object('config.Config')
-    app.register_blueprint(index_routes.index_bp)
-    app.register_blueprint(connected_routes.connected_bp, url_prefix='/connected')
-    app.register_blueprint(admin_routes.admin_bp, url_prefix='/admin')
+
     db.init_app(app)
 
     with app.app_context():
+        from .admin import admin_routes
+        from .connected import connected_routes
+        from .index import index_routes
+
+        app.register_blueprint(index_routes.index_bp)
+        app.register_blueprint(connected_routes.connected_bp, url_prefix='/connected')
+        app.register_blueprint(admin_routes.admin_bp, url_prefix='/admin')
         db.create_all()
         return app
--- application/models.py
+++ application/models.py
@@ -1,4 +1,4 @@
-from application import db
+from . import db
 from sqlalchemy import PrimaryKeyConstraint
 
 
