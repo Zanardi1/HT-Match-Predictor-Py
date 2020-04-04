a17d0749d8a76095545d9315a4a497577aefeb9b
Bogdan Doicin <alex.zanardi@gmail.com>
2020-04-04 14:36:52 +0300

    Actualizare biblioteci

--- application/admin/admin_routes.py
+++ application/admin/admin_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 from flask import render_template
+from application.admin import delete_db
 
 admin_bp = Blueprint('admin_bp', __name__, template_folder='templates', static_folder='static')
 
@@ -11,5 +12,4 @@ def admin():
 
 @admin_bp.route('/delete')
 def delete():
-    from application.admin import delete_db
     delete_db.delete_database()
