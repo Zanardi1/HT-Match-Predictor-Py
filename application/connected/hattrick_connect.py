44e367bccedcbb5f5d0543e43447e895cae39f28
Bogdan Doicin <alex.zanardi@gmail.com>
2020-04-12 15:49:50 +0300

    Lucrez la conectarea la Hattrick

--- application/connected/hattrick_connect.py
+++ application/connected/hattrick_connect.py
@@ -1,6 +1,7 @@
+import webbrowser
+
 import easygui
 from authlib.integrations.requests_client import OAuth1Session
-import webbrowser
 
 CONSUMER_KEY = 'Cx7jQlJTOwIRA2XVtRWF19'
 CONSUMER_SECRET = 'L1gwHsZZuGneTUwZKUevgBUe3HKIMBDiJv7DHwf1RjI'
@@ -14,9 +15,12 @@ ACCESS_TOKEN_PATH = 'https://chpp.hattrick.org/oauth/access_token.ashx'
 
 
 def ConnectionEngine():
-    oauth = OAuth1Session(client_id=CONSUMER_KEY, client_secret=CONSUMER_SECRET, redirect_uri=REQUEST_TOKEN_PATH)
+    oauth = OAuth1Session(client_id=CONSUMER_KEY, client_secret=CONSUMER_SECRET,
+                          callback_uri=REQUEST_TOKEN_PATH, redirect_uri=REQUEST_TOKEN_PATH)
     request_token = oauth.fetch_request_token(REQUEST_TOKEN_PATH)
     path = oauth.create_authorization_url(AUTHORIZE_PATH, request_token=request_token['oauth_token'])
-    webbrowser.open(path)
-    verifier = easygui.enterbox(msg='Enter the PIN provided by the Hattrick site', title='Enter PIN', strip=True)
-    access_token = oauth.fetch_access_token(ACCESS_TOKEN_PATH, verifier=verifier)
+    webbrowser.open(path, new=2)
+    # verifier = easygui.enterbox(msg='Enter the PIN provided by the Hattrick site', title='Enter PIN', strip=True)
+    path2 = oauth.parse_authorization_response(path)
+    easygui.msgbox(path2)
+    access_token = oauth.fetch_access_token(ACCESS_TOKEN_PATH, verifier=path2['oauth_verifier'])
