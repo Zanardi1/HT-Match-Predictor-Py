06361a4372ae81cd2a866a70f5acc52de9c59a75
Bogdan Doicin <alex.zanardi@gmail.com>
2020-05-10 12:23:39 +0300

    Am inceput adaugarea unei ferestre de progres la importul meciurilor

--- application/admin/import_matches.py
+++ application/admin/import_matches.py
@@ -4,11 +4,28 @@ from application.xml import create_string as cs
 from application.xml import dl_xml_file as dl
 from application.xml import xml_parsing as xp
 from application import config
+import tkinter as tk
+from tkinter.ttk import Progressbar, Button
+from multiprocessing import Process, Queue
+
+
+def show_progress_window(low_end, high_end, match_counter):
+    root = tk.Tk()
+    progress_bar = Progressbar(root, mode='determinate', orient='horizontal', length=200)
+    progress_bar['maximum'] = high_end - low_end
+    progress_bar['value'] = match_counter
+    progress_bar.pack()
+    cancel_button = Button(root, text='Anulare')
+    cancel_button.pack()
+    root.mainloop()
 
 
 def import_engine(low_end, high_end):
     file = config['DEFAULT']['PROTECTED_RESOURCE_PATH']
     for match_id in range(low_end, high_end + 1, 1):
+        p = Process(target=show_progress_window, args=(low_end, high_end, match_id - low_end))
+        p.start()
+        p.join()
         params = cs.create_match_details_string(match_id)
         dl.download_xml_file(file, params, global_library.details_savepath)
         match_details = xp.parse_match_details_file(match_id)
