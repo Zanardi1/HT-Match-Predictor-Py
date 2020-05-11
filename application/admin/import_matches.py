bd50aff820a8f6d5765c6ebb5ebd093eb3928652
Bogdan Doicin <alex.zanardi@gmail.com>
2020-05-11 15:51:21 +0300

    Am inceput adaugarea unei ferestre de progres la importul meciurilor

--- application/admin/import_matches.py
+++ application/admin/import_matches.py
@@ -10,23 +10,23 @@ from application.xml import dl_xml_file as dl
 from application.xml import xml_parsing as xp
 
 
-def show_progress_window(low_end, high_end, match_count):
-    root = tk.Tk()
-    progress_bar = Progressbar(root, mode='determinate', orient='horizontal', length=200)
+def show_progress_window(low_end, high_end, match_count, window):
+    progress_bar = Progressbar(window, mode='determinate', orient='horizontal', length=200)
     progress_bar['maximum'] = high_end - low_end
     progress_bar['value'] = match_count
     progress_bar.pack()
-    cancel_button = Button(root, text='Anulare')
+    cancel_button = Button(window, text='Anulare')
     cancel_button.pack()
-    root.mainloop()
 
 
 def import_engine(low_end, high_end):
+    import easygui
     file = config['DEFAULT']['PROTECTED_RESOURCE_PATH']
+    root = tk.Tk()
     for match_id in range(low_end, high_end + 1, 1):
-        p = Process(target=show_progress_window, args=(low_end, high_end, match_id - low_end))
+        easygui.msgbox(match_id)
+        p = Process(target=show_progress_window, args=(low_end, high_end, match_id - low_end, root))
         p.start()
-        p.join()
         params = cs.create_match_details_string(match_id)
         dl.download_xml_file(file, params, global_library.details_savepath)
         match_details = xp.parse_match_details_file(match_id)
@@ -34,3 +34,4 @@ def import_engine(low_end, high_end):
                       match_details[5], match_details[6], match_details[7], match_details[8], match_details[9],
                       match_details[10], match_details[11], match_details[12], match_details[13], match_details[14],
                       match_details[15], match_details[16])
+    root.mainloop()
