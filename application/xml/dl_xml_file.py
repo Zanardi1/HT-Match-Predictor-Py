24876f9708e1c8f6cf789a214daf2483946e1ff6
Bogdan Doicin <alex.zanardi@gmail.com>
2020-05-13 16:16:46 +0300

    Teste de imbunatatire a vitezei de executie a rutinei de adaugare in BD

--- application/xml/dl_xml_file.py
+++ application/xml/dl_xml_file.py
@@ -1,15 +1,11 @@
 # Subrutinele necesare descarcarii unui fisier XML din Hattrick
 
-import configparser as c
-
 from rauth import OAuth1Session
 
-import global_library
+from application import config
 
 
 def download_xml_file(file, params, destination_file):
-    config = c.ConfigParser()
-    config.read(global_library.configuration_file)
     session = OAuth1Session(consumer_key=config['DEFAULT']['CONSUMER_KEY'],
                             consumer_secret=config['DEFAULT']['CONSUMER_SECRET'],
                             access_token=config['DEFAULT']['ACCESS_TOKEN'],
